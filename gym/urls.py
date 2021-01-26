bee32e5341104dac11d52f90c27735e75ce14e2e
gkpatil0311 <gkpatil0311@gmail.com>
2021-01-26 20:50:39 +0530

    added login user authenticate logiv

--- gym/urls.py
+++ gym/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.login, name = 'login'),   
+    #path('',views.about, name = 'about'),    
     path('about/',views.about, name = 'about'),   
     path('contact/',views.contact, name = 'contact'),
-] 
+    path('',views.index, name = 'home'),   
+    path('admin_login',views.login, name = 'login'),
+    path('logout',views.logout_admin, name = 'logout'),
+]
--- gym/views.py
+++ gym/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout,login
+
+
 # Create your views here.
 def about(request):
     return render(request,'About.html')
 def contact(request):
     return render(request,'contact.html')
 def login(request):
-    return render(request,'login.html')
+    error=""
+    if request.method=='POST':
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        user = authenticate(username =u , password=p)
+    
+        try:
+            if user.is_staff:
+                login(request,user)
+                error="no"
+            else:
+                error="yes"
+        except:
+            error="yes"
+    d={'error':error}
+    return render(request,'login.html',d)
+
+def index(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    return render(request,'index.html')
+
+
+def logout_admin(request): 
+    if not request.user.is_staff:
+        return redirect('login')
+    logout(request)
+    return redirect('login')
